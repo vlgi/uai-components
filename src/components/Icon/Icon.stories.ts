ea6d6fd527db9269a96f22333fad38dec3e51f9a
Jose Luiz <joseluizgnogueira@live.com>
2022-01-29 09:33:20 -0300

    package (test): update storyshot test

--- src/components/Icon/Icon.stories.ts
+++ src/components/Icon/Icon.stories.ts
@@ -14,7 +14,6 @@ export default {
     creevey: {
       skip: true,
     },
-    storyshots: false,
   },
 };
 
@@ -37,3 +36,10 @@ export const Default = Template({
 export const List = (): unknown => ({
   Component: IconList,
 });
+
+// disabled storyshots for the list
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+List.parameters = {
+  ...List.parameters,
+  storyshots: false,
+};
