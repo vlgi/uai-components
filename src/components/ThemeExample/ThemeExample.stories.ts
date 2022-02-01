16dbf8678c32265a1995fa5c40015dc2348baebb
Jose Luiz <joseluizgnogueira@live.com>
2022-02-01 07:54:39 -0300

    theme (docs): add more description to theme doc

--- src/components/ThemeExample/ThemeExample.stories.ts
+++ src/components/ThemeExample/ThemeExample.stories.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
-import ThemeExample from "./ThemeExample.svelte";
 import ThemeColors from "./components/Colors/Colors.svelte";
 import ThemeSizes from "./components/Sizes/Sizes.svelte";
-import ThemeFonts from "./components/Fonts/Fonts.svelte"
+import ThemeFonts from "./components/Fonts/Fonts.svelte";
 import ThemeSpacings from "./components/Spacings/Spacings.svelte";
 import ThemeSurfaces from "./components/Surfaces/Surfaces.svelte";
 
 export default {
   title: "Theme",
-  component: ThemeExample,
+  component: ThemeColors,
   parameters: {
     // skip all screen test for button stories
     creevey: {
@@ -19,10 +18,6 @@ export default {
     storyshots: false,
   },
 };
-
-export const Intro = () => ({
-  Component: ThemeExample,
-});
 
 export const Colors = () => ({
   Component: ThemeColors,
