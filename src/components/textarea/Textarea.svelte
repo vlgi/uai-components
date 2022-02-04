efd290365c31dea073096a71de1f2ba1e20cfe6e
Victor André De Moraes <victorandre957@gmail.com>
2022-02-04 12:09:04 +0000

    remove the disable from lint

--- src/components/textarea/Textarea.svelte
+++ src/components/textarea/Textarea.svelte
@@ -105,7 +105,7 @@
     value = x;
   };
 
-  /* eslint-disable */
+
   function getScrollHeight(elm) {
     let savedValue = elm.value;
     elm.value = "";
@@ -129,7 +129,7 @@
       elm.rows = maxRows;
     }
   }
-  /* eslint-enable */
+
   document.addEventListener("input", onExpandableTextareaInput);
 </script>
 
