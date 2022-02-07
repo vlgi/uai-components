08fec3e9df39b97221b3fea098ebdac98996e42c
Jose Luiz <joseluizgnogueira@live.com>
2022-02-07 07:12:50 -0300

    badge (refactor): remove unecessary css

--- src/components/badge/Badge.svelte
+++ src/components/badge/Badge.svelte
@@ -10,9 +10,6 @@
 
 <style lang="scss">
   .badge {
-    position: relative;
-    max-width: var(--theme-fields-max-width);
-    height: var(--theme-fields-height);
     padding-left: var(--theme-fields-padding);
     padding-right: var(--theme-fields-padding);
     text-align: center;
@@ -20,7 +17,6 @@
     border: var(--szot-border,none);
     border-radius: var(--theme-large-shape);
 
-    font-family: var(--theme-font-family);
     font-style: var(--theme-p-font-style);
     font-weight: var(--theme-p-font-weight);
     font-size: var(--theme-p-font-size);
@@ -28,28 +24,29 @@
 
     background-color: var(--szot-background-color );
     color: var(--szot-color,#5F5F5F);
-  }
-  .primary {
-    --szot-background-color: var(--theme-primary);
-    --szot-color: var(--theme-txt-on-primary-surface);
-  }
 
-  .secondary {
-    --szot-background-color: var(--theme-secondary);
-    --szot-color: var(--theme-txt-on-secondary-surface);
-  }
+    &.primary {
+      --szot-background-color: var(--theme-primary);
+      --szot-color: var(--theme-txt-on-primary-surface);
+    }
 
-  .dark {
-    --szot-background-color: var(--theme-dark);
-    --szot-color: var(--theme-txt-on-dark-surface);
-  }
+    &.secondary {
+      --szot-background-color: var(--theme-secondary);
+      --szot-color: var(--theme-txt-on-secondary-surface);
+    }
 
-  .light {
-    --szot-background-color: var(--theme-light);
-    --szot-color: var(--theme-txt-on-light-surface);
-  }
-  .outline {
-    border: var(--szot-border, 0.0563rem solid #5F5F5F);
+    &.dark {
+      --szot-background-color: var(--theme-dark);
+      --szot-color: var(--theme-txt-on-dark-surface);
+    }
+
+    &.light {
+      --szot-background-color: var(--theme-light);
+      --szot-color: var(--theme-txt-on-light-surface);
+    }
+    &.outline {
+      border: var(--szot-border, 0.0563rem solid #5F5F5F);
+    }
   }
 
 </style>
