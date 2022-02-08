b8c5a1c0035162f8ae65f127a58333368c23f8f2
Jose Luiz <joseluizgnogueira@live.com>
2022-02-08 07:55:33 -0300

    side menu (fix): disable storyshots for the draggable button

--- src/components/SideMenu/DraggableButton/DraggableButton.stories.ts
+++ src/components/SideMenu/DraggableButton/DraggableButton.stories.ts
@@ -4,6 +4,13 @@ import DraggableButton from "./DraggableButton.svelte";
 export default {
   title: "Components/SideMenu/Components/DraggableButton",
   component: DraggableButton,
+  parameters: {
+    // skip all screen test for button stories
+    creevey: {
+      skip: true,
+    },
+    storyshots: false,
+  },
 };
 
 const Template = (_args: ArgType) => {
