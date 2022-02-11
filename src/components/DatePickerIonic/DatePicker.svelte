422b6a4aca8f90d5b41d1a35290a2c7e57dabc2c
Victor André de Moraes <victorandre957@gmail.com>
2022-02-11 18:04:56 -0300

    DatePicker (feat) create date picker and add its dependencies

---
+++ src/components/DatePickerIonic/DatePicker.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { format } from "date-fns";
+  import Input from "../formFields/Input/Input.svelte";
+
+  type TinputStyle = "primary" | "secondary" | "dark" | "light";
+  type Tpresentation = "date" | "date-time" | "month" | "month-year" | "time" | "time-date" | "year";
+
+  /** choose default theme colors */
+  export let inputStyle: TinputStyle = "primary";
+  export let icon = "calendar";
+  /** picker display format */
+  export let presentation: Tpresentation = "date";
+  /** date with formatting */
+  export let value = null;
+  /** minimum date to select */
+  export let min: string = null;
+  /** maximum date to select */
+  export let max: string = null;
+  /** unformatted date (read-only) */
+  export let output: Date = null;
+  /** picker confirmation text */
+  export let doneText = "Confirmar";
+  /** picker cancel text */
+  export let cancelText = "Cancelar";
+  /** date format in input */
+  export let formatDate = "dd/MM/yyyy";
+
+
+  const updateDate = (event: InputEvent) => {
+    const data = (event.target as HTMLInputElement).value;
+    value = format(new Date(data), formatDate);
+    output = new Date(data);
+  };
+
+</script>
+<!-- Datetime in popover with input -->
+<div>
+    <Input
+    {inputStyle}
+    {...$$restProps}
+    bind:value
+    id="open-date-input-2"
+    {icon}
+    />
+      <ion-popover trigger="open-date-input-2" show-backdrop="false">
+        <ion-datetime
+          color={inputStyle}
+          {min}
+          {max}
+          {presentation}
+          done-text={doneText}
+          cancel-text={cancelText}
+          mode="md"
+          show-default-buttons={true}
+          id="popover-datetime-2"
+          on:ionChange={updateDate}/>
+      </ion-popover>
+</div>
+
+<style lang="scss">
+  div {
+    --szot-icon-margin: 0.5rem 1rem;
+  }
+  ion-popover {
+    height: 30rem;
+  }
+  ion-datetime {
+    --ion-safe-area-right: 0;
+  }
+
+</style>
--- src/ionic-components.ts
+++ src/ionic-components.ts
@@ -1,6 +1,23 @@
 import { IonItem } from "@ionic/core/components/ion-item";
 import { IonLabel } from "@ionic/core/components/ion-label";
 import { IonInput } from "@ionic/core/components/ion-input";
+import { IonPopover } from "@ionic/core/components/ion-popover";
+import { IonDatetime } from "@ionic/core/components/ion-datetime";
+import { IonCard } from "@ionic/core/components/ion-card";
+import { IonIcon } from "@ionic/core/components/ion-icon";
+import { IonButton } from "@ionic/core/components/ion-button";
+import { IonButtons } from "@ionic/core/components/ion-buttons";
+import { IonSelectOption } from "@ionic/core/components/ion-select-option";
+import { IonPicker } from "@ionic/core/components/ion-picker";
+import { IonContent } from "@ionic/core/components/ion-content";
+import { IonModal } from "@ionic/core/components/ion-modal";
+
+import { IonPickerInternal } from "@ionic/core/components/ion-picker-internal";
+import { IonPickerColumnInternal } from "@ionic/core/components/ion-picker-column-internal";
+import { IonRippleEffect } from "@ionic/core/components/ion-ripple-effect";
+import { IonNote } from "@ionic/core/components/ion-note";
+import { IonBackdrop } from "@ionic/core/components/ion-backdrop";
+
 import { initialize } from "@ionic/core/components";
 import * as helperCustomElements from "./helpers/custom-elements";
 
@@ -11,3 +28,19 @@ initialize({
 helperCustomElements.define("ion-item", IonItem);
 helperCustomElements.define("ion-label", IonLabel);
 helperCustomElements.define("ion-input", IonInput);
+helperCustomElements.define("ion-popover", IonPopover);
+helperCustomElements.define("ion-datetime", IonDatetime);
+helperCustomElements.define("ion-picker", IonPicker);
+helperCustomElements.define("ion-card", IonCard);
+helperCustomElements.define("ion-icon", IonIcon);
+helperCustomElements.define("ion-button", IonButton);
+helperCustomElements.define("ion-buttons", IonButtons);
+helperCustomElements.define("ion-select-option", IonSelectOption);
+helperCustomElements.define("ion-content", IonContent);
+helperCustomElements.define("ion-modal", IonModal);
+
+helperCustomElements.define("ion-picker-internal", IonPickerInternal);
+helperCustomElements.define("ion-picker-column-internal", IonPickerColumnInternal);
+helperCustomElements.define("ion-ripple-effect", IonRippleEffect);
+helperCustomElements.define("ion-note", IonNote);
+helperCustomElements.define("ion-backdrop", IonBackdrop);
