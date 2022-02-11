ed2c6ed077e69b8530d761688aae5224503f4d09
Jose Luiz <joseluizgnogueira@live.com>
2022-02-11 19:25:44 -0300

    modal (docs): create storybooks

---
+++ src/components/Modal/Modal.stories.ts
@@ -0,0 +1,42 @@
+import type { ArgType } from "@storybook/addons";
+import Modal from "./Modal.svelte";
+import ModalForTest from "./ModalForTest.svelte";
+import ModalForInceptionTest from "./ModalForInceptionTest.svelte";
+import ModalForCustomScrollTest from "./ModalForCustomScrollTest.svelte";
+
+export default {
+  title: "Components/Modal",
+  component: Modal,
+};
+
+const Template = (_args: ArgType) => {
+  const ret = ({ ...props }) => ({
+    Component: ModalForTest,
+    props,
+  });
+  ret.args = _args;
+  return ret;
+};
+
+export const Default = Template({
+  opened: true,
+});
+
+const content = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil velit distinctio incidunt? Maiores ratione libero dolorum recusandae cum inventore officia ipsam, sunt cupiditate doloribus, reiciendis architecto praesentium ullam impedit necessitatibus?";
+const largeContent = Array(40).fill(content).join("\n");
+export const LongContent = Template({
+  opened: true,
+  content: largeContent,
+});
+
+export const Closed = Template({
+  opened: false,
+});
+
+export const ModalInsideModal = () => ({
+  Component: ModalForInceptionTest,
+});
+
+export const ModalInsideDivScroll = () => ({
+  Component: ModalForCustomScrollTest,
+});
---
+++ src/components/Modal/ModalForInceptionTest.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Modal from "./Modal.svelte";
+
+  export let disableHeader = false;
+  const openeds = [false, false, false];
+
+  function openModal(index: number) {
+    openeds[index] = true;
+  }
+</script>
+
+<button on:click={() => openModal(0)}>Open 1</button>
+
+<!-- Modal 1 -->
+<Modal bind:opened={openeds[0]} {disableHeader} --szot-width="400px" --szot-height="400px">
+  <div slot="modal-header">
+    <span>My modal header</span>
+  </div>
+  <div slot="modal-content">
+    <button on:click={() => openModal(1)}>Open 2</button><br>
+
+    <!-- Modal 2 -->
+    <Modal bind:opened={openeds[1]} {disableHeader} --szot-width="300px" --szot-height="300px">
+      <div slot="modal-content">
+        <button on:click={() => openModal(2)}>Open 3</button><br>
+
+        <!-- Modal 3 -->
+        <Modal bind:opened={openeds[2]} {disableHeader} --szot-width="200px" --szot-height="200px">
+          <div slot="modal-content">
+            Hi
+          </div>
+        </Modal>
+      </div>
+    </Modal>
+
+  </div>
+  <div slot="modal-footer">
+    <span>My modal footer</span>
+  </div>
+</Modal>
+
+<div class="scroll-maker"></div>
+
+<style global lang="scss">
+  .scroll-maker {
+    height: 150vh;
+    width: 150vw;
+  }
+</style>
