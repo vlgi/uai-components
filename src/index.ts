83bfa0ce16456b7e82d641a551795cfc9f6361c4
Jose Luiz <joseluizgnogueira@live.com>
2022-02-10 21:23:39 -0300

    package (feat): add search input to the package

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { default as Form } from "./components/Form/Form.svelte";
 export { default as SideMenu } from "./components/SideMenu/SideMenu.svelte";
 
 export { default as Select } from "./components/formFields/Select/Select.svelte";
+export { default as SearchInput } from "./components/formFields/Select/SearchInput/SearchInput.svelte";
 export { default as Input } from "./components/formFields/Input/Input.svelte";
 export { default as Button } from "./components/formFields/Button/Button.svelte";
 export { default as Textarea } from "./components/formFields/Textarea/Textarea.svelte";
