7dcb8f6a03ae1986b11d77f8fd8aaa465a9d448d
Jose Luiz <joseluizgnogueira@live.com>
2022-02-12 08:13:08 -0300

    dialog (docs): create stories

---
+++ src/components/Dialog/Dialog.stories.ts
@@ -0,0 +1,50 @@
+import type { ArgType } from "@storybook/addons";
+import Dialog from "./Dialog.svelte";
+
+export default {
+  title: "Components/Dialog",
+  component: Dialog,
+  parameters: {
+    docs: {
+      page: null,
+    },
+  },
+  argTypes: {
+    type: {
+      control: {
+        type: "select",
+        options: ["confirm", "confirmCancel", "info", "error", "warning", "success"],
+      },
+    },
+  },
+  args: {
+    opened: true,
+    title: "my dialog title",
+    content: "hi! please confirme to go on.",
+  },
+};
+
+const Template = (_args: ArgType) => {
+  const ret = ({ ...props }) => ({
+    Component: Dialog,
+    props,
+  });
+  ret.args = _args;
+  return ret;
+};
+
+export const Default = Template({
+});
+
+export const WithCloseOnEscOrClickOut = Template({
+  disableCloseOnEvents: false,
+});
+
+export const Confirm = Template({
+  type: "confirm",
+});
+
+export const Error = Template({
+  type: "error",
+  content: "Sorry, something goes wrong.",
+});
