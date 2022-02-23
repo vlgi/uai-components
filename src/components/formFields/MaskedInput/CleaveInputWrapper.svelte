7ef780dbc319e51130536d75a50d11aed357143c
Victor André Gris Costa <victorandr98@gmail.com>
2022-02-23 11:29:50 -0300

    CleaveInput (test): create tests

---
+++ src/components/formFields/MaskedInput/CleaveInputWrapper.svelte
@@ -0,0 +1,39 @@
+<script>
+
+import Form from "../../Form/Form.svelte";
+import CleaveInput from "./CleaveInput.svelte";
+
+export let inputElement;
+export let isValid;
+export let isAllValid;
+export let value;
+export let values;
+/**
+ * @type {CleaveInput}
+ */
+export let inputComponent;
+
+</script>
+
+<Form bind:values bind:isAllValid>
+  <CleaveInput
+    bind:this={inputComponent}
+    bind:inputElement
+    bind:isValid
+    bind:value
+    {...$$restProps}
+  />
+</Form>
+
+<p>
+  <b>isValid:</b> { isValid }
+</p>
+<p>
+  <b>value:</b> { JSON.stringify(value) }
+</p>
+<p>
+  <b>isAllValid:</b> { isAllValid }
+</p>
+<p>
+  <b>Form values:</b> { JSON.stringify(values) }
+</p>
