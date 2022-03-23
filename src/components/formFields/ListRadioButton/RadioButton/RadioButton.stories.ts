86ae9fc58e2daa724a0ea819cb803a56e15e1116
fernando.jsts <fernando.jsts@gmail.com>
2022-03-23 11:14:06 -0300

    radio button (fix): Radio button needs ID, now it exists on docs.

--- src/components/formFields/ListRadioButton/RadioButton/RadioButton.stories.ts
+++ src/components/formFields/ListRadioButton/RadioButton/RadioButton.stories.ts
@@ -19,12 +19,21 @@ const Template = (_args: ArgType) => {
   return ret;
 };
 
-export const Default = Template({});
+export const Default = Template({
+  id: "radioId",
+});
 
 export const DefaultChecked = Template({
   checked: true,
+  id: "radioId",
 });
 
 export const Filled = Template({
   filled: true,
+  id: "radioId",
+});
+
+export const Labeled = Template({
+  id: "radioId",
+  label: "Item",
 });
