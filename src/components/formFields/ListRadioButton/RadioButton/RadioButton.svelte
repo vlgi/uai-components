086bc5a75be7959d4ff21bb18d09d9e270db1505
fernando.jsts <fernando.jsts@gmail.com>
2022-03-23 08:02:57 -0300

    radio button (style): Change styling code for radio item

--- src/components/formFields/ListRadioButton/RadioButton/RadioButton.svelte
+++ src/components/formFields/ListRadioButton/RadioButton/RadioButton.svelte
@@ -7,38 +7,67 @@
   export let checked: boolean;
 </script>
 
-<input
-  {...$$restProps}
-  type="radio"
-  {name}
-  {id}
-  {value}
-  class:filled
-  class="radio-input"
-  {checked}
-/>
+<div class="radio-item">
+  <input
+    {...$$restProps}
+    type="radio"
+    {name}
+    {id}
+    {value}
+    class:filled
+    class="radio-input"
+    {checked}
+  />
 
-<label for={id} class="radio-label">{label !== undefined ? label : ""}</label>
+  <label for={id} class="radio-label">{label !== undefined ? label : ""}</label>
+</div>
 
 <style>
-  .radio-input:after {
-    width: 15px;
-    height: 15px;
-    border-radius: 15px;
-    top: -2px;
-    left: -1px;
-    position: relative;
-    background-color: none;
-    content: "";
+  .radio-item {
     display: inline-block;
-    border: var(--szot-border-size, 2px) solid var(--szot-border-color, #5f5f5f);
+    position: relative;
+    padding: 0 6px;
+    margin: 10px 0 0;
   }
 
-  .filled:checked:after {
+  .radio-item input[type="radio"] {
+    display: none;
+  }
+
+  .radio-item label {
+    color: #666;
+    font-weight: normal;
+  }
+
+  .radio-item label:before {
+    content: " ";
+    display: inline-block;
+    position: relative;
+    top: 5px;
+    margin: 0 5px 0 0;
+    width: var(--szot-radio-size, 16px);
+    height: var(--szot-radio-size, 16px);
+    border-radius: calc(var(--szot-radio-size, 16px)/2);
+    border: 2px solid var(--szot-radio-color, var(--szot-border-color, #5f5f5f));
+    background-color: transparent;
+  }
+
+  .radio-item input[type="radio"]:checked + label:after {
+    width: calc(var(--szot-radio-size, 16px) - 8px);
+    height: calc(var(--szot-radio-size, 16px) - 8px);
+    border-radius: calc(var(--szot-radio-size, 16px)/2);
+    position: absolute;
+    top: 9px;
+    left: 10px;
+    content: " ";
+    display: block;
+    background: var(--szot-radio-color, var(--szot-border-color, #5f5f5f));
+  }
+
+  .filled:checked + label:before {
     background-color: var(
       --szot-radio-color,
       var(--szot-border-color, #5f5f5f)
     );
-    border-color: var(--szot-radio-color, var(--szot-border-color, #5f5f5f));
   }
 </style>
