930c62ceb582667bba4076c5e9562d356f47df27
Victor André Gris Costa <victorandr98@gmail.com>
2022-04-14 14:35:50 -0300

    storybook (refactor): change webpack configuration to resolve relative scss urls

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
   "stories": [
-    "../CODE_OF_CONDUCT/**/*.stories.mdx",
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
   ],
@@ -15,6 +14,25 @@ module.exports = {
   "framework": "@storybook/svelte",
   "svelteOptions": {
     "preprocess": require("svelte-preprocess")()
+  },
+  webpackFinal: async (config) => {
+    // =========== Edit sass rule ============ //
+    /**
+     * @type { {test: RegExp, use: {loader: string, options: any}[]} }
+     */
+    const sass = config.module.rules.find(e => e.test.test(".scss"));
+
+    sass.use.splice(2, 0, {
+      loader: "resolve-url-loader",
+    });
+    if (sass.use[3].options) {
+      sass.use[3].options.sourceMap = true;
+    } else {
+      sass.use[3].options = { sourceMap: true };
+    }
+
+    // Return the altered config
+    return config;
   },
   core: {
     builder: 'webpack5',
