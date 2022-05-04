4c1101d6d4dd2efeeab276e12ab3443e18e13efa
DaniloRaposo <danilo-raposo@hotmail.com>
2022-05-04 15:01:45 -0300

    BadgePillClickable (docs): add stories to component

---
+++ src/components/formFields/ListCheckboxStyled/BadgePillClickable/BadgePillCickableWrapper.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import BadgePillClickable from "./BadgePillClickable.svelte";
+
+  type TIconPosition = "left" | "right" | "both" | "none";
+  type TBadgeStyle = "light" | "dark";
+
+  export let label = "";
+
+  export let checked = false;
+
+  export let iconPosition: TIconPosition = "none";
+
+  export let badgeStyle: TBadgeStyle = "dark";
+
+  export let style = "";
+</script>
+
+<div {style}>
+  <BadgePillClickable
+    {label}
+    name="test"
+    {checked}
+    {iconPosition}
+    {badgeStyle}
+    {...$$restProps}
+  />
+</div>
