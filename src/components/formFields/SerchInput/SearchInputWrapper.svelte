997b14ac72eab425bb492f8c63ab31183a671afe
DaniloRaposo <danilo-raposo@hotmail.com>
2022-05-20 11:37:18 -0300

    SearchInput (docs): create component wrapper

---
+++ src/components/formFields/SerchInput/SearchInputWrapper.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import SearchInput from "./SearchInput.svelte";
+
+  export let inputElement: HTMLInputElement;
+  export let focus: ()=> void;
+  export let searchQuery = "";
+  export let filtered: unknown[] = [];
+  export let items: unknown[] = [];
+  export let searchable: string[] = [];
+</script>
+
+<SearchInput
+  bind:inputElement
+  bind:focus
+  bind:searchQuery
+  bind:filtered
+  {items}
+  {searchable}
+  name="search"
+  {...$$restProps}
+/>
+
+<header>
+  <b>Results</b>
+</header>
+{#each filtered as filter}
+  <p>{JSON.stringify(filter)}</p>
+{/each}
