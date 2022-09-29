8601e21e0957f7bdf72f01130e70ea24eafc052c
Victor André De Moraes <victorandre957@gmail.com>
2022-09-29 18:04:18 +0000

    Feature/changing the color of the checkbox and radiobutton when checked

--- src/components/formFields/ListRadioButton/RadioButton/RadioButton.stories.ts
+++ src/components/formFields/ListRadioButton/RadioButton/RadioButton.stories.ts
@@ -42,3 +42,10 @@ export const Labeled = Template({
   value: "labeled",
   group: "labeled",
 });
+
+export const LargeLabeled = Template({
+  label: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  name: "labeled",
+  value: "labeled",
+  group: "",
+});
