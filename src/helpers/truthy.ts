c3939fbf067c6b9d6c385372b94ec03a3539d5b2
Danilo Raposo <daniloraposocaldas@hotmail.com>
2023-03-21 17:42:58 -0300

    helper (feat): add helper functions

---
+++ src/helpers/truthy.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+const falsy = [
+  false,
+  null,
+  undefined,
+  0,
+  NaN,
+  "",
+  // document.all,
+  BigInt(0),
+  -0,
+];
+
+export function isFalsy(value: any): boolean {
+  if (Array.isArray(value)) return !value.length;
+  return falsy.includes(value);
+}
+export function isTruthy(value: any): boolean {
+  return !isFalsy(value);
+}
+
+const empty = [
+  null,
+  undefined,
+];
+
+export function isEmpty(value: any): boolean {
+  return empty.includes(value);
+}
