8241a553ed7cf3fe1c7b503359177cf8b983bc9d
01oseluiz <joseluizgnogueira@live.com>
2023-05-30 18:27:31 -0300

    storybook (fix): add suport to github-flavored-markdown

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,22 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
+import remarkGfm from "remark-gfm";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      // https://storybook.js.org/docs/react/writing-docs/mdx#lack-of-github-flavored-markdown-gfm
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
+  ],
   framework: {
     name: "@storybook/sveltekit",
     options: {},
