9777b23b4fb67ea0a550196cb5e3324ff3c24099
Danilo Raposo <daniloraposocaldas@hotmail.com>
2023-05-30 11:18:15 +0000

    Feature/create range slider

---
+++ src/components/formFields/RangeSlider/RangeSliderWrapper.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import RangeSlider from "./RangeSlider.svelte";
+
+  export let style = "";
+
+  export let value = 0;
+</script>
+
+<div class="wrapper" {style}>
+  <RangeSlider name="range" bind:value {...$$restProps} on:input/>
+
+  <p>value: {value}</p>
+</div>
