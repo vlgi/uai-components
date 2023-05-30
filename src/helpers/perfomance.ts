4eb563e25e7aaa40450da1f4d4d1c252d1fdfe89
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-05-30 13:39:27 +0000

    Emoji Picker

---
+++ src/helpers/perfomance.ts
@@ -0,0 +1,35 @@
+/**
+ * The throttle function checks the current time and compares it
+ * to the last time the function was called,
+ * if the difference is greater than the wait time,
+ * the original function is called and the time is updated.
+ *
+ * @param fn
+ * is the function that we want to limit the rate of calls to.
+ * @param waitTime
+ * time period in milliseconds that we want to limit the rate of calls to the function to.
+ * @returns
+ * Returns the function that we want to execute
+ */
+
+export function throttle
+  <T extends(...args: unknown[])=> unknown>(fn: T, waitTime: number): T {
+  let timer = null;
+  let lastExec = null;
+
+  function returnFn(...args): void {
+    if (!lastExec) {
+      fn.apply(args);
+      lastExec = Date.now();
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        if ((Date.now() - lastExec) >= waitTime) {
+          fn.apply(args);
+          lastExec = Date.now();
+        }
+      }, waitTime - (Date.now() - lastExec));
+    }
+  }
+  return returnFn as T;
+}
