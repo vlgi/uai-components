704c66947624a31b927baeccba79f3679cf4728b
01oseluiz <joseluizgnogueira@live.com>
2023-05-31 14:28:11 -0300

    collapsable (refactor): update stories

--- src/lib/components/Collapsable/Collapsable.stories.ts
+++ src/lib/components/Collapsable/Collapsable.stories.ts
@@ -1,141 +1,120 @@
 import { bounceOut } from "svelte/easing";
-import type { ArgType } from "@storybook/addons";
-import Collapsable from "./Collapsable.svelte";
-import CollapsableExample from "./CollapsableExample.svelte";
-import CollapsableWrapper from "./CollapsableWrapper.svelte";
+import type { Meta } from "@storybook/svelte";
+import type { TemplatedStoryObj, TemplateObj } from "$types/storybook";
 
-export default {
+import Collapsable from "./Collapsable.svelte";
+import CollapsableRealExample from "./CollapsableRealExample.svelte";
+import CollapsableExample from "./CollapsableExample.svelte";
+import CollapsableRealExampleString from "./CollapsableRealExample.svelte?raw";
+
+/**
+ * The new version of Storybook broke this documentation.
+ * Storybook can't create docs for components with context module.
+ *
+ * We will work on this documentation problem.
+ */
+const meta = {
   title: "Components/Collapsable",
   component: Collapsable,
+  tags: ["autodocs"], // enable auto docs
+} satisfies Meta<Collapsable>;
+
+export default meta;
+
+type Template = TemplateObj<typeof meta, CollapsableExample>;
+type Story = TemplatedStoryObj<typeof meta, CollapsableExample>;
+
+const template = {
+  render: (args) => ({
+    Component: CollapsableExample,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+    title: "My Collapsable",
+  },
   argTypes: {
-    style: {
-      control: {
-        type: "text",
-      },
-    },
     collapsableStyle: {
-      control: {
-        type: "select",
-        options: ["primary", "secondary", "dark", "light"],
-      },
+      control: "select",
+      options: ["primary", "secondary", "dark", "light"],
     },
     collapsableStyleType: {
-      control: {
-        type: "select",
-        options: ["filled", "outline", "not-filled"],
-      },
+      control: "select",
+      options: ["filled", "outline", "not-filled"],
     },
-    contentBackground: {
-      control: {
-        type: "boolean",
-      },
-    },
-    expanded: {
-      control: {
-        type: "boolean",
-      },
+  },
+} satisfies Template;
+
+export const Default: Story = {
+  ...template,
+  args: {
+    // args for this specific story here
+    ...template.args,
+    contentBackground: false,
+    expanded: false,
+  },
+};
+
+export const ContentBackground: Story = {
+  ...template,
+  args: {
+    // args for this specific story here
+    ...template.args,
+    contentBackground: true,
+    expanded: false,
+  },
+};
+
+export const DifferentAnimation: Story = {
+  ...template,
+  args: {
+    // args for this specific story here
+    ...template.args,
+    contentBackground: true,
+    expanded: false,
+    expansionTransitionParams: {
+      duration: 1000,
+      delay: 500,
+      easing: bounceOut,
     },
   },
 };
 
-const TemplateSingle = (_args: ArgType, code?: string) => {
-  const ret = ({ ...props }) => ({
-    Component: CollapsableWrapper,
-    props,
-  });
-  ret.args = _args;
-  if (code) {
-    ret.parameters = {
-      docs: {
-        source: {
-          language: "html",
-          code,
-        },
-      },
-    };
-  }
-  return ret;
-};
-const Template = (_args: ArgType, code?: string) => {
-  const ret = ({ ...props }) => ({
-    Component: CollapsableExample,
-    props,
-  });
-  ret.args = _args;
-  if (code) {
-    ret.parameters = {
-      docs: {
-        source: {
-          language: "html",
-          code,
-        },
-      },
-    };
-  }
-  return ret;
-};
-
-export const Default = TemplateSingle({
-  contentBackground: false,
-  expanded: false,
-});
-
-export const ContentBackground = TemplateSingle({
-  title: "My Collapsable",
-  contentBackground: true,
-  expanded: false,
-});
-
-export const DifferentAnimation = TemplateSingle({
-  title: "My Collapsable",
-  contentBackground: true,
-  expanded: false,
-  expansionTransitionParams: {
-    duration: 1000,
-    delay: 500,
-    easing: bounceOut,
+export const CustomExample: Story = {
+  ...template,
+  args: {
+    // args for this specific story here
+    ...template.args,
+    contentBackground: true,
+    expanded: false,
+    collapsableStyleType: "outline",
+    style:
+      "--szot-collapsable-border-width: 10px;" +
+      "--szot-collapsable-border-color: linear-gradient(to right, red, blue, green);" +
+      "--szot-collapsable-color: linear-gradient(to right, red, blue, green);" +
+      "--szot-collapsable-content-border-width: 5px;" +
+      "--szot-collapsable-content-border-color: linear-gradient(to bottom left, red, orange, yellow, green, blue, indigo, violet);" +
+      "--szot-collapsable-content-color: linear-gradient(to bottom left, red, orange, yellow, green, blue, indigo, violet);" +
+      "--szot-collapsable-icon-color: linear-gradient(to top, magenta, yellow, cyan);" +
+      "--szot-collapsable-title-font-size: 24px;" +
+      "--szot-collapsable-box-shadow: inset -0.5px -1px 1px rgba(0, 0, 0, 0.13), inset 0.5px 1px 1px rgba(255, 255, 255, 0.02);" +
+      "--szot-collapsable-backdrop-filter: blur(30.5px);",
   },
-});
+};
 
-export const CustomExample = TemplateSingle({
-  title: "My Collapsable",
-  contentBackground: true,
-  expanded: false,
-  collapsableStyleType: "outline",
-  style: "--szot-collapsable-border-width: 10px;"
-  + "--szot-collapsable-border-color: linear-gradient(to right, red, blue, green);"
-  + "--szot-collapsable-color: linear-gradient(to right, red, blue, green);"
-  + "--szot-collapsable-content-border-width: 5px;"
-  + "--szot-collapsable-content-border-color: linear-gradient(to bottom left, red, orange, yellow, green, blue, indigo, violet);"
-  + "--szot-collapsable-content-color: linear-gradient(to bottom left, red, orange, yellow, green, blue, indigo, violet);"
-  + "--szot-collapsable-icon-color: linear-gradient(to top, magenta, yellow, cyan);"
-  + "--szot-collapsable-title-font-size: 24px;"
-  + "--szot-collapsable-box-shadow: inset -0.5px -1px 1px rgba(0, 0, 0, 0.13), inset 0.5px 1px 1px rgba(255, 255, 255, 0.02);"
-  + "--szot-collapsable-backdrop-filter: blur(30.5px);",
-});
-
-export const Accordion = Template({}, `
-<script lang="ts">
-  import Collapsable from "./Collapsable.svelte";
-</script>
-
-<Collapsable title="My Collapsable Item 1" group="my-group">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. A non accusamus cum qui omnis hic vitae accusantium dignissimos, labore totam fugiat illo quaerat tenetur inventore dolorum ullam distinctio nihil tempore!
-</Collapsable>
-
-<Collapsable title="My Collapsable Item 2" group="my-group">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. A non accusamus cum qui omnis hic vitae accusantium dignissimos, labore totam fugiat illo quaerat tenetur inventore dolorum ullam distinctio nihil tempore!
-</Collapsable>
-
-<Collapsable title="My Collapsable Item 3" group="my-group">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. A non accusamus cum qui omnis hic vitae accusantium dignissimos, labore totam fugiat illo quaerat tenetur inventore dolorum ullam distinctio nihil tempore!
-</Collapsable>
-
-<Collapsable title="My Collapsable Item 4" group="my-group">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. A non accusamus cum qui omnis hic vitae accusantium dignissimos, labore totam fugiat illo quaerat tenetur inventore dolorum ullam distinctio nihil tempore!
-</Collapsable>
-
-<Collapsable title="My Collapsable Item 5" group="my-group">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. A non accusamus cum qui omnis hic vitae accusantium dignissimos, labore totam fugiat illo quaerat tenetur inventore dolorum ullam distinctio nihil tempore!
-</Collapsable>
-`);
+/**
+ * This story must be the last one, because storybook autodocs get the
+ * first story to extract the props.
+ */
+export const RealExample: TemplatedStoryObj<typeof meta, CollapsableRealExample> = {
+  render: () => ({
+    Component: CollapsableRealExample,
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: CollapsableRealExampleString,
+      },
+    },
+  },
+};
