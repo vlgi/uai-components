f043356f384a98e507650e86e7e7bfc8df3dc497
VitorLamego <vitorlamegoo@gmail.com>
2023-05-31 17:03:23 -0300

    Icon (refactor): update stories

--- src/lib/components/Icon/Icon.stories.ts
+++ src/lib/components/Icon/Icon.stories.ts
@@ -1,44 +1,47 @@
-import type { ArgType } from "@storybook/addons";
-import { action } from "@storybook/addon-actions";
-import Icon from "./Icon.svelte";
-import Wrapper from "./IconWrapper.svelte";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj, TemplateObj } from "$types/storybook";
 
-export default {
+import Icon from "./Icon.svelte";
+import IconWrapper from "./IconWrapper.svelte";
+
+const meta = {
   title: "Components/Icon",
   component: Icon,
-  argTypes: {
-    onClick: { action: "onClick" },
+  tags: ["autodocs"], // enable auto docs
+  // common stuff for all stories here
+} satisfies Meta<Icon>;
+
+export default meta;
+
+type Template = TemplateObj<typeof meta, IconWrapper>;
+type Story = StoryObj<typeof meta, IconWrapper>;
+
+const template = {
+  render: (args) => ({
+    Component: IconWrapper,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
   },
-  parameters: {
-    // skip all screen test for this stories
-    creevey: {
-      skip: true,
-    },
+} satisfies Template;
+
+export const Default: Story = {
+  ...template,
+  args: {
+    iconName: "mdi:cellphone",
+    ...template.args,
   },
 };
 
-const Template = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: Wrapper,
-    props,
-    on: {
-      click: action("onClick"),
-    },
-  });
-  ret.args = _args;
-  return ret;
+export const Custom: Story = {
+  ...template,
+  args: {
+    iconName: "ic:baseline-apple",
+    style: "--szot-icon-font-size: 80px;" + "--szot-icon-color: red;",
+    ...template.args,
+  },
 };
-
-export const Default = Template({
-  iconName: "mdi:cellphone",
-});
-
-export const Custom: any = Template({
-  iconName: "ic:baseline-apple",
-  style:
-      "--szot-icon-font-size: 80px;"
-      + "--szot-icon-color: red;",
-});
 
 Custom.parameters = {
   docs: {
