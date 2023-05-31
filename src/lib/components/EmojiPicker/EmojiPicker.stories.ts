a103850b1821e2c292de56523b22ee0a995c1442
VitorLamego <vitorlamegoo@gmail.com>
2023-05-31 17:31:47 -0300

    EmojiPicker (refactor): update stories

--- src/lib/components/EmojiPicker/EmojiPicker.stories.ts
+++ src/lib/components/EmojiPicker/EmojiPicker.stories.ts
@@ -1,82 +1,104 @@
-import type { ArgType } from "@storybook/addons";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj, TemplateObj } from "$types/storybook";
+
 import EmojiPicker from "./EmojiPicker.svelte";
 import EmojiPickerWrapper from "./EmojiPickerWrapperExample.svelte";
-import EmojiPickerWithModal from "./EmojiPickerWithModalExample.svelte";
-import EmojiPickerWithDropdown from "./EmojiPickerWithDropdownExample.svelte";
+import EmojiPickerWithModalExample from "./EmojiPickerWithModalExample.svelte";
+import EmojiPickerWithDropdownExample from "./EmojiPickerWithDropdownExample.svelte";
 
-export default {
+const meta = {
   title: "Components/EmojiPicker",
   component: EmojiPicker,
-  argTypes: {},
-};
+  tags: ["autodocs"], // enable auto docs
+  // common stuff for all stories here
+} satisfies Meta<EmojiPicker>;
 
-const Template = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
+export default meta;
+
+type Template = TemplateObj<typeof meta, EmojiPickerWrapper>;
+type Story = StoryObj<typeof meta, EmojiPickerWrapper>;
+
+const template = {
+  render: (args) => ({
     Component: EmojiPickerWrapper,
-    props,
-  });
-  ret.args = _args;
-  return ret;
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+  },
+} satisfies Template;
+
+export const Default: Story = {
+  ...template,
+  args: {
+    opened: true,
+    maxRecently: 45,
+    style:
+      "--szot-emojis-picker-background-color: white;" +
+      "\n--szot-emojis-picker-border-radius: 25px;" +
+      "\n--szot-emojis-picker-height: 315px;" +
+      "\n--szot-emojis-picker-width: 315px;" +
+      "\n--szot-emojis-picker-category-title-border-color: #ddd;" +
+      "\n--szot-emojis-picker-category-title-font-color: #444;" +
+      "\n--szot-emojis-picker-tabs-border-color: #eee;" +
+      "\n--szot-emojis-picker-tabs-icon-color: #777;",
+    ...template.args,
+  },
 };
 
-export const Default = Template({
-  opened: true,
-  maxRecently: 45,
-  style:
-    "--szot-emojis-picker-background-color: white;"
-    + "\n--szot-emojis-picker-border-radius: 25px;"
-    + "\n--szot-emojis-picker-height: 315px;"
-    + "\n--szot-emojis-picker-width: 315px;"
-    + "\n--szot-emojis-picker-category-title-border-color: #ddd;"
-    + "\n--szot-emojis-picker-category-title-font-color: #444;"
-    + "\n--szot-emojis-picker-tabs-border-color: #eee;"
-    + "\n--szot-emojis-picker-tabs-icon-color: #777;",
-});
+const templateWithModal = {
+  render: (args) => ({
+    Component: EmojiPickerWithModalExample,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+  },
+} satisfies Template;
 
-const TemplateWithModal = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: EmojiPickerWithModal,
-    props,
-  });
-  ret.args = _args;
-  return ret;
+export const WithModal: Story = {
+  ...templateWithModal,
+  args: {
+    opened: true,
+    maxRecently: 45,
+    style:
+      "--szot-emojis-picker-background-color: white;" +
+      "\n--szot-emojis-picker-border-radius: 0;" +
+      "\n--szot-emojis-picker-box-shadow: auto;" +
+      "\n--szot-emojis-picker-height: 400px;" +
+      "\n--szot-emojis-picker-width: 320px;" +
+      "\n--szot-emojis-picker-category-title-border-color: #ddd;" +
+      "\n--szot-emojis-picker-category-title-font-color: #444;" +
+      "\n--szot-emojis-picker-tabs-border-color: #eee;" +
+      "\n--szot-emojis-picker-tabs-icon-color: #777;",
+    ...templateWithModal.args,
+  },
 };
 
-export const WithModal = TemplateWithModal({
-  opened: true,
-  maxRecently: 45,
-  style:
-    "--szot-emojis-picker-background-color: white;"
-    + "\n--szot-emojis-picker-border-radius: 0;"
-    + "\n--szot-emojis-picker-box-shadow: auto;"
-    + "\n--szot-emojis-picker-height: 400px;"
-    + "\n--szot-emojis-picker-width: 320px;"
-    + "\n--szot-emojis-picker-category-title-border-color: #ddd;"
-    + "\n--szot-emojis-picker-category-title-font-color: #444;"
-    + "\n--szot-emojis-picker-tabs-border-color: #eee;"
-    + "\n--szot-emojis-picker-tabs-icon-color: #777;",
-});
+const templateWithDropdown = {
+  render: (args) => ({
+    Component: EmojiPickerWithDropdownExample,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+  },
+} satisfies Template;
 
-const TemplateWithDropdown = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: EmojiPickerWithDropdown,
-    props,
-  });
-  ret.args = _args;
-  return ret;
+export const WithDropdown: Story = {
+  ...templateWithDropdown,
+  args: {
+    opened: true,
+    maxRecently: 45,
+    style:
+      "--szot-emojis-picker-background-color: #FFFCF5;" +
+      "\n--szot-emojis-picker-border-radius: 0;" +
+      "\n--szot-emojis-picker-box-shadow: auto;" +
+      "\n--szot-emojis-picker-height: 400px;" +
+      "\n--szot-emojis-picker-width: 320px;" +
+      "\n--szot-emojis-picker-category-title-border-color: #ddd;" +
+      "\n--szot-emojis-picker-category-title-font-color: #444;" +
+      "\n--szot-emojis-picker-tabs-border-color: #eee;" +
+      "\n--szot-emojis-picker-tabs-icon-color: #777;",
+  },
 };
-
-export const WithDropdown = TemplateWithDropdown({
-  opened: true,
-  maxRecently: 45,
-  style:
-    "--szot-emojis-picker-background-color: #FFFCF5;"
-    + "\n--szot-emojis-picker-border-radius: 0;"
-    + "\n--szot-emojis-picker-box-shadow: auto;"
-    + "\n--szot-emojis-picker-height: 400px;"
-    + "\n--szot-emojis-picker-width: 320px;"
-    + "\n--szot-emojis-picker-category-title-border-color: #ddd;"
-    + "\n--szot-emojis-picker-category-title-font-color: #444;"
-    + "\n--szot-emojis-picker-tabs-border-color: #eee;"
-    + "\n--szot-emojis-picker-tabs-icon-color: #777;",
-});
