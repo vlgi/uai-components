d4a16ab921154fc877e0e11758b136f8f479a590
01oseluiz <joseluizgnogueira@live.com>
2023-05-31 16:58:25 -0300

    dropdown (refactor): update stories

--- src/lib/components/Dropdown/Dropdown.stories.ts
+++ src/lib/components/Dropdown/Dropdown.stories.ts
@@ -1,126 +1,114 @@
-import type { ArgType } from "@storybook/addons";
-import Dropdown from "./Dropdown.svelte";
-import DropdownTest from "./DropdownTest.svelte";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj, TemplateObj } from "$types/storybook";
 
-export default {
+import Dropdown from "./Dropdown.svelte";
+import DropdownExample from "./DropdownExample.svelte";
+
+const meta = {
   title: "Components/Dropdown",
   component: Dropdown,
   argTypes: {
     dropdownAlignment: {
-      control: {
-        type: "select",
-        options: [
-          "leftTop",
-          "leftBottom",
-          "rightTop",
-          "rightBottom",
-          "topLeft",
-          "topRight",
-          "bottomLeft",
-          "bottomRight",
-        ],
-      },
+      control: "select",
+      options: [
+        "leftTop",
+        "leftBottom",
+        "rightTop",
+        "rightBottom",
+        "topLeft",
+        "topRight",
+        "bottomLeft",
+        "bottomRight",
+      ],
     },
+  },
+  // common stuff for all stories here
+} satisfies Meta<Dropdown>;
+
+export default meta;
+
+type Template = TemplateObj<typeof meta, DropdownExample>;
+type Story = StoryObj<typeof meta, DropdownExample>;
+
+const template = {
+  render: (args) => ({
+    Component: DropdownExample,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+    targetId: "trigger-button",
+  },
+} satisfies Template;
+
+export const Default: Story = {
+  ...template,
+  args: {
+    // args for this specific story here
+    ...template.args,
+    dropdownAlignment: "bottomRight",
   },
 };
 
-const Template = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: DropdownTest,
-    props,
-  });
-  ret.args = _args;
-  return ret;
-};
-
-export const Default: any = Template({
-  triggerId: "Default",
-  dropdownAlignment: "bottomRight",
-  opened: true,
-});
-
-Default.parameters = {
-  docs: {
-    source: {
-      language: "html",
-      code: `
-<script lang="ts">
-  import Dropdown from "./Dropdown.svelte";
-  import Button from "../formFields/Button/Button.svelte";
-</script>
-
-<div class="button-wrapper" id="demo-dropdown">
-  <Button>Open</Button>
-</div>
-
-<Dropdown targetId="demo-dropdown">
-  <span class="item">Item 1</span>
-  <span class="item">Item 2</span>
-  <span class="item">Item 3 Long text</span>
-  <span class="item">Item 4</span>
-  <span class="item">Item 5 Long Long Long text</span>
-</Dropdown>
-
-<style lang="scss">
-  .item {
-    display: block;
-  }
-
-  .button-wrapper {
-    width: fit-content;
-    height: fit-content;
-  }
-</style>
-      `,
-    },
+export const RightAlignment: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "rightTop",
   },
 };
 
-export const OpenClose = Template({
-  triggerId: "OpenClose",
-  dropdownAlignment: "bottomRight",
-  opened: false,
-});
+export const LeftAlignment: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "leftTop",
+  },
+};
 
-export const RightAlignment = Template({
-  triggerId: "RightAlignment",
-  dropdownAlignment: "rightTop",
-  opened: true,
-});
+export const BottomAlignment: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "bottomLeft",
+    triggerStyle: "top:290px; left:0",
+  },
+};
 
-export const LeftAlignment = Template({
-  triggerId: "LeftAlignment",
-  dropdownAlignment: "leftTop",
-  opened: true,
-});
+export const RightAlignmentWithAutoAdjust: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "rightTop",
+    triggerStyle: "top:0; left:250px",
+  },
+};
 
-export const BottomAlignment = Template({
-  triggerId: "BottomAlignment",
-  dropdownAlignment: "rightTop",
-  triggerStyle: "top:290px; left:0",
-  opened: true,
-});
+export const RightAlignmentWithoutAutoAdjust: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "rightTop",
+    triggerStyle: "top:0; left:250px",
+    enableAutAdjust: false,
+  },
+};
 
-export const RightAlignmentWithAutoAdjust = Template({
-  triggerId: "RightAlignmentWithAutoAdjust",
-  dropdownAlignment: "rightTop",
-  triggerStyle: "top:0; left:250px",
-  opened: true,
-});
-
-export const RightAlignmentWithoutAutoAdjust = Template({
-  triggerId: "RightAlignmentWithoutAutoAdjust",
-  dropdownAlignment: "rightTop",
-  triggerStyle: "top:0; left:250px",
-  opened: true,
-  enableAutAdjust: false,
-});
-
-export const largeScroll = Template({
-  triggerId: "largeScroll",
-  dropdownAlignment: "rightTop",
-  triggerStyle: "top:100vw; left:100vw",
-  scrollStyle: "width:250vw; height:250vh",
-  containerStyle: "width:calc(100vw - 120px); height:calc(99vh - 120px); margin: 50px",
-  opened: true,
-});
+/**
+ * Use this to see how dropdown works on scroll containers
+ */
+export const largeScroll: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    dropdownAlignment: "rightTop",
+    triggerStyle: "top:100vw; left:100vw",
+    scrollStyle: "width:250vw; height:250vh",
+    containerStyle: "width:calc(100vw - 120px); height:calc(99vh - 120px); margin: 50px",
+  },
+  parameters: {
+    controls: {
+      include: ["dropdownAlignment", "opened", "enableAutAdjust"],
+    },
+  },
+};
--- src/lib/components/Dropdown/DropdownExample.svelte
+++ src/lib/components/Dropdown/DropdownExample.svelte
@@ -1,26 +1,38 @@
 <script lang="ts">
+  import type { ComponentProps } from "svelte";
   import Dropdown from "./Dropdown.svelte";
   import Button from "../formFields/Button/Button.svelte";
 
-  export let triggerId: string;
+  // Force this example extend the base component interface, so we can use the
+  // spreed of restProps, instead of declare again all component interface manually.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface $$Props extends ComponentProps<Dropdown> {
+    triggerStyle?: string;
+    scrollStyle?: string;
+    containerStyle?: string;
+  }
+
   export let triggerStyle = "top:0; left:0";
   export let scrollStyle = "";
   export let containerStyle = "width:300px; height:300px; margin: 100px 300px";
+
+  // Used to force $$restProps be of type ComponentProps<Dropdown>;
+  $: restProps = $$restProps as ComponentProps<Dropdown>;
 </script>
 
 <div
   class="container"
   style={containerStyle}
-  >
+>
   <div
     class="button-wrapper"
-    id={triggerId}
+    id={restProps.targetId}
     style={triggerStyle}
   >
     <Button>Open</Button>
   </div>
 
-  <Dropdown targetId={triggerId} {...$$restProps}>
+  <Dropdown {...restProps}>
     <span class="item">Item 1</span>
     <span class="item">Item 2</span>
     <span class="item">Item 3 Long text</span>
@@ -31,7 +43,7 @@
   <div
     class="add-scroll"
     style={scrollStyle}
-  ></div>
+  />
 </div>
 
 <style lang="scss">
