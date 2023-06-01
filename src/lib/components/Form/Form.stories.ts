63d14d7f1391530008667b64f5c07a1370e3ce78
VitorLamego <vitorlamegoo@gmail.com>
2023-06-01 16:17:17 -0300

    Form (refactor): update stories

--- src/lib/components/Form/Form.stories.ts
+++ src/lib/components/Form/Form.stories.ts
@@ -1,14 +1,15 @@
-import { action } from "@storybook/addon-actions";
-import type { Meta, StoryObj } from "@storybook/svelte";
-import Form from "./Form.svelte";
-import CustomUsageExampleComponent from "./examples/CustomUsageExample.svelte";
-import CustomInputExampleComponent from "./examples/CustomInputExample.svelte";
-import CustomButtonExampleComponent from "./examples/CustomButtonExample.svelte";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj } from "$types/storybook";
+import CustomInputExample from "./examples/CustomInputExample.svelte";
+import CustomButtonExample from "./examples/CustomButtonExample.svelte";
+import CustomUsageExample from "./examples/CustomUsageExample.svelte";
 import FormFieldsComponent from "./examples/FormFieldsExample.svelte";
 import FormFieldsFilledComponent from "./examples/FormFieldsExampleFilled.svelte";
 import CookieFormExampleComponent from "./examples/CookieFormExample.svelte";
 import FormSubmitFunctionExampleComponent from "./examples/FormSubmitFunctionExample.svelte";
 import FormSubmitOutsideExampleComponent from "./examples/FormSubmitOutsideExample.svelte";
+
+import Form from "./Form.svelte";
 
 const meta = {
   title: "Components/Form",
@@ -18,181 +19,173 @@ const meta = {
 } satisfies Meta<Form>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
-};
+export const CustomInput: StoryObj<typeof meta, CustomInputExample> = {
+  render: (props) => ({
+    Component: CustomInputExample,
+    props,
+  }),
+  parameters: {
+    docs: {
+      source: {
+        language: "html",
+        code: `
+      <script lang="ts">
+        import {
+          onMount, getContext, hasContext, onDestroy,
+        } from "svelte";
+        import type { TFormContext } from "./types";
 
-export const CustomInputExample = () => ({
-  Component: CustomInputExampleComponent,
-  on: {
-    input: action("on:input"),
-    change: action("on:change"),
-  },
-});
+        export let name: string;
+        export let value = "";
+        export let required = false;
 
-CustomInputExample.parameters = {
-  docs: {
-    source: {
-      language: "html",
-      code: `
-<script lang="ts">
-  import {
-    onMount, getContext, hasContext, onDestroy,
-  } from "svelte";
-  import type { TFormContext } from "./types";
+        let inputEl: HTMLInputElement;
+        let isValid: boolean;
 
-  export let name: string;
-  export let value = "";
-  export let required = false;
+        const isInsideContext = hasContext("FormContext");
+        const {
+          setFieldValue,
+          addFieldToContext,
+          removeFieldFromContext,
+        } = isInsideContext && getContext<TFormContext>("FormContext");
 
-  let inputEl: HTMLInputElement;
-  let isValid: boolean;
+        function validateInput(_value) {
+          if (_value === "my-input") return true;
+          return false;
+        }
 
-  const isInsideContext = hasContext("FormContext");
-  const {
-    setFieldValue,
-    addFieldToContext,
-    removeFieldFromContext,
-  } = isInsideContext && getContext<TFormContext>("FormContext");
+        onMount(() => {
+          if (isInsideContext) {
+            addFieldToContext(name, value, isValid, required, inputEl);
+          }
+        });
 
-  function validateInput(_value) {
-    if (_value === "my-input") return true;
-    return false;
-  }
+        onDestroy(() => {
+          if (isInsideContext) {
+            removeFieldFromContext(name);
+          }
+        });
 
-  onMount(() => {
-    if (isInsideContext) {
-      addFieldToContext(name, value, isValid, required, inputEl);
-    }
-  });
+        // check inputEl to run only after mounted
+        $: if (inputEl) {
+          isValid = validateInput(value);
+          if (isInsideContext) {
+            setFieldValue(name, value, isValid);
+          }
+        }
+      </script>
 
-  onDestroy(() => {
-    if (isInsideContext) {
-      removeFieldFromContext(name);
-    }
-  });
-
-  // check inputEl to run only after mounted
-  $: if (inputEl) {
-    isValid = validateInput(value);
-    if (isInsideContext) {
-      setFieldValue(name, value, isValid);
-    }
-  }
-</script>
-
-<input
-  type="text"
-  { name }
-  { required }
-  bind:this={ inputEl }
-  bind:value
-/>
-{#if !isValid}
-  <p>Invalid! Type "my-input"</p>
-{/if}
-`,
-    },
-  },
-};
-
-export const CustomButtonExample = () => ({
-  Component: CustomButtonExampleComponent,
-  on: {
-    click: action("on:click"),
-  },
-});
-
-CustomButtonExample.parameters = {
-  docs: {
-    source: {
-      language: "html",
-      code: `
-<script lang="ts">
-  import { getContext, hasContext } from "svelte";
-
-  import type { TFormContext } from "./types";
-
-  const isInsideContext = hasContext("FormContext");
-  const { fireSubmit } = isInsideContext && getContext<TFormContext>("FormContext");
-
-</script>
-
-<button on:click={ () => isInsideContext && fireSubmit() } on:click>
-  Submit
-</button>
+      <input
+        type="text"
+        { name }
+        { required }
+        bind:this={ inputEl }
+        bind:value
+      />
+      {#if !isValid}
+        <p>Invalid! Type "my-input"</p>
+      {/if}
       `,
+      },
     },
   },
 };
 
-export const CustomUsageExample = () => ({
-  Component: CustomUsageExampleComponent,
-  on: {
-    submit: action("on:submit"),
+export const CustomButton: StoryObj<typeof meta, CustomButtonExample> = {
+  render: (props) => ({
+    Component: CustomButtonExample,
+    props,
+  }),
+  parameters: {
+    docs: {
+      source: {
+        language: "html",
+        code: `
+      <script lang="ts">
+        import { getContext, hasContext } from "svelte";
+
+        import type { TFormContext } from "./types";
+
+        const isInsideContext = hasContext("FormContext");
+        const { fireSubmit } = isInsideContext && getContext<TFormContext>("FormContext");
+
+      </script>
+
+      <button on:click={ () => isInsideContext && fireSubmit() } on:click>
+        Submit
+      </button>
+            `,
+      },
+    },
   },
-});
+};
 
-CustomUsageExample.parameters = {
-  docs: {
-    source: {
-      language: "html",
-      code: `
-<script lang="ts">
-  import Input from "./CustomInputExample.svelte";
-  import Button from "./CustomButtonExample.svelte";
-  import Form from "./Form.svelte";
+export const CustomUsage: StoryObj<typeof meta, CustomUsageExample> = {
+  render: (props) => ({
+    Component: CustomUsageExample,
+    props,
+  }),
+  parameters: {
+    docs: {
+      source: {
+        language: "html",
+        code: `
+        <script lang="ts">
+          import Input from "./CustomInputExample.svelte";
+          import Button from "./CustomButtonExample.svelte";
+          import Form from "./Form.svelte";
 
-  export let values: Record<string, unknown>;
-  export let isAllValid: boolean;
-</script>
+          export let values: Record<string, unknown>;
+          export let isAllValid: boolean;
+        </script>
 
-<Form bind:values bind:isAllValid on:submit>
-  <Input name="input-name"/>
-  <br>
-  <Button/>
-</Form>
+        <Form bind:values bind:isAllValid on:submit>
+          <Input name="input-name"/>
+          <br>
+          <Button/>
+        </Form>
 
-<p><b>Form Values:</b> { JSON.stringify(values) }</p>
-<p><b>Form valid:</b> { isAllValid }</p>
+        <p><b>Form Values:</b> { JSON.stringify(values) }</p>
+        <p><b>Form valid:</b> { isAllValid }</p>
 `,
+      },
     },
   },
 };
 
-export const FormFields = () => ({
-  Component: FormFieldsComponent,
-  on: {
-    submit: action("on:submit"),
-  },
-});
+export const FormFields: StoryObj<typeof meta, FormFieldsComponent> = {
+  render: (props) => ({
+    Component: FormFieldsComponent,
+    props,
+  }),
+};
 
-export const FormFieldsFilled = () => ({
-  Component: FormFieldsFilledComponent,
-  on: {
-    submit: action("on:submit"),
-  },
-});
+export const FormFieldsFilled: StoryObj<typeof meta, FormFieldsFilledComponent> = {
+  render: (props) => ({
+    Component: FormFieldsFilledComponent,
+    props,
+  }),
+};
 
-export const CookieFormExample = () => ({
-  Component: CookieFormExampleComponent,
-  on: {
-    submit: action("on:submit"),
-  },
-});
+export const CookieForm: StoryObj<typeof meta, CookieFormExampleComponent> = {
+  render: (props) => ({
+    Component: CookieFormExampleComponent,
+    props,
+  }),
+};
 
-export const FormSubmitFunctionExample = () => ({
-  Component: FormSubmitFunctionExampleComponent,
-  on: {
-    submit: action("on:submit"),
-  },
-});
+export const FormSubmitFunctionExample: StoryObj<typeof meta, FormSubmitFunctionExampleComponent> =
+  {
+    render: (props) => ({
+      Component: FormSubmitFunctionExampleComponent,
+      props,
+    }),
+  };
 
-export const FormSubmitOutsideExample = () => ({
-  Component: FormSubmitOutsideExampleComponent,
-  on: {
-    submit: action("on:submit"),
-  },
-});
+export const FormSubmitOutsideExample: StoryObj<typeof meta, FormSubmitOutsideExampleComponent> = {
+  render: (props) => ({
+    Component: FormSubmitOutsideExampleComponent,
+    props,
+  }),
+};
