bfd10ad87a00602382504c84fe9a4ac1a8b1e59f
Danilo Raposo <daniloraposocaldas@hotmail.com>
2023-06-01 14:44:38 -0300

    Button (docs): adjust button component to refactor in szot ui

--- src/lib/components/formFields/Button/Button.stories.ts
+++ src/lib/components/formFields/Button/Button.stories.ts
@@ -1,186 +1,232 @@
-import type { ArgType } from "@storybook/addons";
-import { action } from "@storybook/addon-actions";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj, TemplateObj } from "$types/storybook";
 import Button from "./Button.svelte";
-import ButtonWrapper from "./ButtonWrapperForTest.svelte";
+import ButtonExample from "./ButtonExample.svelte";
 
-export default {
+
+const meta = {
   title: "Components/FormFields/Button",
   component: Button,
+  tags: ["autodocs"], // enable auto docs
   argTypes: {
-    onClick: { action: "on:click" },
     buttonStyle: {
-      control: {
-        type: "select",
-        options: ["primary", "secondary", "dark", "light"],
-      },
+      control: "select",
+      options: ["primary", "secondary", "dark", "light"],
     },
     size: {
-      control: {
-        type: "select",
-        options: ["small", "medium", "large", "round"],
-      },
+      control: "select",
+      options: ["small", "medium", "large", "round"],
     },
     buttonStyleType: {
-      control: {
-        type: "select",
-        options: ["filled", "outline", "not-filled"],
-      },
+      control: "select",
+      options: ["filled", "outline", "not-filled"],
     },
     positionIcon: {
-      control: {
-        type: "select",
-        options: ["left", "right"],
-      },
+      control: "select",
+      options: ["left", "right"],
     },
     type: {
-      control: {
-        type: "select",
-        options: ["submit", "reset", "button"],
-      },
+      control: "select",
+      options: ["submit", "reset", "button"],
     },
   },
-};
+} satisfies Meta<Button>;
 
-const Template = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: ButtonWrapper,
-    props,
-    on: {
-      click: action("on:click"),
-    },
-  });
-  ret.args = _args;
-  return ret;
-};
+export default meta;
 
-export const ThemeTest = Template({
-  style: "",
-  size: "medium",
-  labelSlot: "Botão 1",
-  buttonStyle: "secondary",
-});
+type Template = TemplateObj<typeof meta, ButtonExample>;
+type Story = StoryObj<typeof meta, ButtonExample>;
 
-export const LargeSimple = Template({
-  size: "large",
-  labelSlot: "Minhas contas",
-});
+const template = {
+  render: (args) => ({
+    Component: ButtonExample,
+    props: args,
+  }),
+  args: {},
+} satisfies Template;
 
-export const LargeIcon = Template({
-  size: "large",
-  icon: "mdi:upload",
-  labelSlot: "Enviar documentos",
-  positionIcon: "right",
-  style: "--szot-button-max-width: 20rem",
-});
+export const ThemeTest: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "medium",
+    labelSlot: "Botão 1",
+    buttonStyle: "secondary",
+  }
+}
 
-export const mediumSimple = Template({
-  size: "medium",
-  labelSlot: "Botão 1",
-});
+export const LargeSimple: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "large",
+    labelSlot: "Minhas contas",
+  }
+}
 
-export const mediumIcon = Template({
-  size: "medium",
-  icon: "mdi:file",
-  labelSlot: "Documento",
-  positionIcon: "right",
-});
+export const LargeIcon: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "large",
+    icon: "mdi:upload",
+    labelSlot: "Enviar documentos",
+    positionIcon: "right",
+    style: "--szot-button-max-width: 20rem",
+  }
+}
 
-export const smallSimple = Template({
-  size: "small",
-  labelSlot: "Buscar",
-});
+export const MediumSimple: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "medium",
+    labelSlot: "Botão 1",
+  }
+}
 
-export const smallIcon = Template({
-  size: "small",
-  icon: "mdi:magnify",
-  labelSlot: "Buscar",
-});
+export const MediumIcon: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "medium",
+    icon: "mdi:file",
+    labelSlot: "Documento",
+    positionIcon: "right",
+  }
+}
 
-export const Disabled = Template({
-  size: "large",
-  disabled: "true",
-  labelSlot: "Bloquear",
-});
+export const SmallSimple: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "small",
+    labelSlot: "Buscar",
+  }
+}
 
-export const NotBackground = Template({
-  size: "large",
-  labelSlot: "Bloquear",
-  buttonStyleType: "not-filled",
-});
+export const SmallIcon: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "small",
+    icon: "mdi:magnify",
+    labelSlot: "Buscar",
+  }
+}
 
-export const NotBackgroundDisabled = Template({
-  size: "large",
-  buttonStyleType: "not-filled",
-  disabled: "true",
-  labelSlot: "Bloquear",
-});
+export const Disabled: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "large",
+    disabled: true,
+    labelSlot: "Bloquear",
+  }
+}
 
-export const withBorders = Template({
-  buttonStyleType: "outline",
-  size: "medium",
-  labelSlot: "Bloquear",
-});
+export const NotBackground: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "large",
+    labelSlot: "Bloquear",
+    buttonStyleType: "not-filled",
+  }
+}
 
-export const withBordersDisabled = Template({
-  buttonStyleType: "outline",
-  size: "medium",
-  labelSlot: "Bloquear",
-  disabled: "true",
-});
+export const NotBackgroundDisabled: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "large",
+    buttonStyleType: "not-filled",
+    disabled: true,
+    labelSlot: "Bloquear",
+  }
+}
 
-export const Round = Template({
-  style: "",
-  size: "round",
-  icon: "mdi:dots-horizontal",
-});
+export const WithBorders: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    buttonStyleType: "outline",
+    size: "medium",
+    labelSlot: "Bloquear",
+  }
+}
 
-export const RoundNotBackground = Template({
-  size: "round",
-  icon: "mdi:dots-horizontal",
-  buttonStyleType: "not-filled",
-});
+export const WithBordersDisabled: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    buttonStyleType: "outline",
+    size: "medium",
+    labelSlot: "Bloquear",
+    disabled: true,
+  }
+}
 
-export const RoundDisabled = Template({
-  size: "round",
-  icon: "mdi:dots-horizontal",
-  disabled: "true",
-});
+export const Round: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    style: "",
+    size: "round",
+    icon: "mdi:dots-horizontal",
+  }
+}
 
-export const CustomExample: any = Template({
-  labelSlot: "orange",
-  icon: "ic:outline-remove-red-eye",
-  style:
-      "--szot-button-color: #333;"
-      + "--szot-button-background-color: linear-gradient(to bottom, rebeccapurple, steelblue, turquoise);"
-      + "--szot-button-border-radius: 1.5rem;"
-      + "--szot-button-width: 100%;"
-      + "--szot-button-max-width: 100%;"
-      + "--szot-button-icon-color: red;"
-      + "--szot-button-white-space: normal;"
-      + "--szot-button-box-shadow: inset -0.5px -1px 1px rgba(0, 0, 0, 0.13), inset 0.5px 1px 1px rgba(255, 255, 255, 0.02);",
-});
+export const RoundNotBackground: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "round",
+    icon: "mdi:dots-horizontal",
+    buttonStyleType: "not-filled",
+  }
+}
 
-CustomExample.parameters = {
-  docs: {
-    source: {
-      language: "html",
-      code: `
-  <script lang="ts">
-    import Button from "./Button.svelte";
-  </script>
+export const RoundDisabled: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    size: "round",
+    icon: "mdi:dots-horizontal",
+    disabled: true,
+  }
+}
 
-  <div style="--szot-button-color:#ffc7a7;
-      --szot-button-background-color:#FC6627;
-      --szot-button-buttonStyleType: 2px solid #ffc7a7;
-      --szot-button-icon-color: red;
-      --szot-button-border-radius: 1.5rem" >
-    <Button
-      icon="ic:outline-remove-red-eye"
-    >
-      orange
-    </Button>
-  </div>
-  `,
-    },
-  },
-};
+export const CustomExample: Story = {
+  ...template,
+  args: {
+    ...template.args,
+    // args for this specific story here
+    labelSlot: "orange",
+    icon: "ic:outline-remove-red-eye",
+    style:
+        "--szot-button-color: #333;"
+        + "--szot-button-background-color: linear-gradient(to bottom, rebeccapurple, steelblue, turquoise);"
+        + "--szot-button-border-radius: 1.5rem;"
+        + "--szot-button-width: 100%;"
+        + "--szot-button-max-width: 100%;"
+        + "--szot-button-icon-color: red;"
+        + "--szot-button-white-space: normal;"
+        + "--szot-button-box-shadow: inset -0.5px -1px 1px rgba(0, 0, 0, 0.13), inset 0.5px 1px 1px rgba(255, 255, 255, 0.02);",
+  }
+}
