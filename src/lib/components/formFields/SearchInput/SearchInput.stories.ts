c234128e6afd8b3855846c4bbb7acb0d0cb4d3f4
VitorLamego <vitorlamegoo@gmail.com>
2023-06-02 11:32:14 -0300

    SearchInput (refactor): update stories

--- src/lib/components/formFields/SearchInput/SearchInput.stories.ts
+++ src/lib/components/formFields/SearchInput/SearchInput.stories.ts
@@ -1,37 +1,64 @@
-import type { ArgType } from "@storybook/addons";
+import type { Meta } from "@storybook/svelte";
+import type { StoryObj, TemplateObj } from "$types/storybook";
 import { complexDesserts } from "./fixtures";
-import SearchInput from "./SearchInput.svelte";
-import Wrapper from "./SearchInputWrapper.svelte";
 
-export default {
+import SearchInput from "./SearchInput.svelte";
+import SearchInputExample from "./SearchInputWrapper.svelte";
+
+const meta = {
   title: "Components/FormFields/SearchInput",
   component: SearchInput,
+  tags: ["autodocs"], // enable auto docs
+  // common stuff for all stories here
+} satisfies Meta<SearchInput>;
+
+export default meta;
+
+type Template = TemplateObj<typeof meta, SearchInputExample>;
+type Story = StoryObj<typeof meta, SearchInputExample>;
+
+let focus: () => void;
+let inputElement: HTMLInputElement;
+
+const template = {
+  render: (args) => ({
+    Component: SearchInputExample,
+    props: args,
+  }),
+  args: {
+    // common stuff for template stories here
+    inputElement,
+    focus,
+  },
+} satisfies Template;
+
+export const Default: Story = {
+  ...template,
+  args: {
+    items: complexDesserts,
+    searchable: ["variety", "flavor"],
+    searchQuery: "",
+    ...template.args,
+  },
 };
 
-const Template = (_args: ArgType) => {
-  const ret = ({ ...props }) => ({
-    Component: Wrapper,
-    props,
-  });
-  ret.args = _args;
-  return ret;
+export const Searching: Story = {
+  ...template,
+  args: {
+    items: complexDesserts,
+    searchable: ["variety"],
+    searchQuery: "brownie",
+    icon: "mdi:magnify",
+    ...template.args,
+  },
 };
 
-export const Default = Template({
-  items: complexDesserts,
-  searchable: ["variety", "flavor"],
-  searchQuery: "",
-});
-
-export const Searching = Template({
-  items: complexDesserts,
-  searchable: ["variety"],
-  searchQuery: "brownie",
-  icon: "mdi:magnify",
-});
-
-export const MultipleFieldsSearching = Template({
-  items: complexDesserts,
-  searchable: ["variety", "topping", "flavor"],
-  searchQuery: "brownie chocolate",
-});
+export const MultipleFieldsSearching: Story = {
+  ...template,
+  args: {
+    items: complexDesserts,
+    searchable: ["variety", "topping", "flavor"],
+    searchQuery: "brownie chocolate",
+    ...template.args,
+  },
+};
